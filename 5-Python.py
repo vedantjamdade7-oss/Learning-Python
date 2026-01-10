8334ded6c511ac215ee2116955692bda0f5329de
My-Learning-Journey <jamdadevedant7@gmail.com>
2026-01-10 10:59:39 +0530

    Add Inicial Python code

---
+++ 5-Python.py
@@ -0,0 +1,13 @@
+# Conditional statement in python
+
+# 1. if statement
+# if statement works only for True condition
+a = 20
+b = 26
+if b > a:
+    print("b is greater than a")
+
+# 2 with input function
+age = int(input("Enter your age :"))
+if age > 19:
+    print("You are an Adult")
