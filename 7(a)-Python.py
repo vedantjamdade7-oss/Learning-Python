fbf88a201b49d3478f1372f82d33cc89e6a86205
My-Learning-Journey <jamdadevedant7@gmail.com>
2026-01-11 17:26:55 +0530

    Add Inicial Python code

---
+++ 7(a)-Python.py
@@ -0,0 +1,26 @@
+# Loops in python - while & for loop
+
+# while loop
+# While loop - is used to repeat a block of code as long as a certain condition is true.
+
+count = 0
+while count < 5:
+    print(count)
+    count += 1
+
+# print number form 1 to 69
+
+count2 = 1
+while count2 < 6:
+    print(count2)
+    count2 =count2+1
+
+count = 5
+while count > 0:
+    print(count)
+    count -=1
+else:
+    print("While loop ended")
+
+while True:
+    print("yha babyyy")
---
+++ 7(b)-Python.py
@@ -0,0 +1,26 @@
+# # For loop - is used to iterate over a sequence(like a list, tuple, string) or other iterable objects.
+
+# language = "python"
+
+# for x in language:
+#     print(x)
+
+# ranges in python - It is used to generate a sequence of numbers.
+# # range(stop)
+# # range(start,stop)
+# # range(start,stop,step)
+
+
+# for i in range(6):
+#     print(i)
+
+for x in range(5,10):
+    print(x)
+
+# for c in range(1,11,3):
+#     print(c)
+
+for i in range (10):
+    print(i)
+else:
+    print('for loop ended')
