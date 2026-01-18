b3b1cf6ecb082e4bcedd40b64a6feab96d05ebf2
My-Learning-Journey <jamdadevedant7@gmail.com>
2026-01-18 14:34:14 +0530

    Add Inicial Python code

---
+++ 15-Python.py
@@ -0,0 +1,3 @@
+# FILE HANDLING IN PYTHON
+# File handling is an important aspect of programming that allow us to read from and write to filed on our system.
+# In python we have several built-in functions and methods to handle files easily.
