12372e700d3b1911da0708c11bdd9fe6a41900e7
My-Learning-Journey <jamdadevedant7@gmail.com>
2026-01-19 21:59:26 +0530

    Add Inicial Python code

--- 16-Python.py
+++ 16-Python.py
@@ -1,5 +1,8 @@
 #  Module and Package in Python
-# 1️. What is a Module in Python?
+
+# Modules also Known as Libraries in python.
+
+#  What is a Module in Python?
 
 # A module is a single Python file (.py) that contains functions, classes, or variables.
 # It allows you to organize your code into separate files for better maintainability and reusability.
@@ -18,11 +21,55 @@
 # def multiply(a, b):
 #     return a * b
 
-# 2. Importing a Module
+# Importing a Module
 # you can import a module into your python script using the "import" statement.
 
+# import math
+
+import math
 import calculator
 
 print(calculator.add(5, 3))
 print(calculator.subtract(10, 4))
+
+
+a = 36
+print(math.sqrt(a))
+
+# import specific function from library
+
+from math import factorial
+b = 4
+print(factorial(b))
+
+# installed new modules/library
+# pip install <library_name>
+
+# import pandas as pd
+
+# Packages in python
+# A package is a collection of related modules organized in a directory hierarchy.
+
+# Example:
+# Let's say we have a package named 'data_analysis' with the following structure:
+# data_analysis/
+#  __init__.py
+#  statistics.py
+#  visualization.py
+# you can import modules from a package using the dot notation.
+
+# from data_analysis import statistics
+# from data_analysis.visualization import plot_graph
+# In this example, 'data_analysis' is the package and 'statistics and 'visualization' are modules within that package.
+
+# Importing from another module
+from mymodule import person1, person2
+
+print(person1['age'])
+print(person1['name'])
+print(person2['name'])
+print(person2['age'])
+print(person1)
+print(person2)
+
 
---
+++ mymodule.py
@@ -0,0 +1,3 @@
+# mymodule.py (This is a module)
+person1 = {'name' : 'vedant', 'age' : 20}
+person2 = {'name' : 'kunal', 'age' : 25}
